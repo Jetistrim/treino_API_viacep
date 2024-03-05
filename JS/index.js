11d405146c916449ab7d957e4432f58ed9196da3
Jetistrim <paulogabriellima@hotmail.com>
2024-03-05 12:04:59 -0300

    inicio

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+function findAddress(){
+    if(cep.value.length == 8){
+        fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+        .then(response => response.json())
+        .then(response => {
+            logradouro.value = response.logradouro;
+            bairro.value = response.bairro;
+            localidade.value = response.localidade;
+            uf.value = response.uf
+        })
+    }
+}
